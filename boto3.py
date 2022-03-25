65dee504e5ed3e6ed8acb2a6b9a3c040507bf4bb
Developer Mujtaba <mujtabaglacier@gmail.com>
2022-03-25 16:46:39 +0500

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
 from datetime import datetime, timedelta, timezone
 import boto3
 ec2 = boto3.resource('ec2')
 cl = boto3.client('ec2')
 count=0
-def lambda_handler(event, context):
-    ec2=boto3.client('ec2')
-# List (ec2.Snaphot)
-snapshots = ec2.snapshots.filter(OwnerIds=['self'])
+snapshots = ec2.snapshots.filter(OwnerIds=['xxxxxxxxxx'])
 def if_associated_to_ami(client, snapshot_id):
     img = client.describe_images(Filters=[{'Name': 'block-device-mapping.snapshot-id', 'Values': [snapshot_id]}])
     try:
@@ -25,6 +21,7 @@ for snapshot in snapshots:
         delete_time = datetime.now(tz=timezone.utc) - timedelta(days=90)
         if delete_time > start_time:
             #snapshot.delete()
+            cl.delete_snapshot(SnapshotId=snapshot.snapshot_id)
             print('Snapshot with Id = {} is deleted '. format(snapshot.snapshot_id))
     count+=1
     if count == 1000:
